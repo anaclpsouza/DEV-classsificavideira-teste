18aa2dbca6c71c7d237064ec89650982377962e8
Ana Clara Souza <anaclarap03@gmail.com>
2025-07-14 11:40:53 +0000

    commit

--- appy.py
+++ appy.py
@@ -1,1 +1,10 @@
 import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title="Classifica Folhas de Videiras!"
+    )
+
+    st.write("# Classifica Folhas de Videiras")
+
+    if __name__ == "__main__": main()
